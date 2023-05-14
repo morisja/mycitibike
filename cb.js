dc4aa2c7afeb21aff920f33225d4c1cc3acff3b0
James Morris <morisja@gmail.com>
2023-05-13 21:21:40 -0400

    complete

--- cb.js
+++ cb.js
@@ -27,19 +27,6 @@ function sortByKey(array, key) {
   });
 }
 
-function getFavourites() {
-  stations = localStorage["mystations"];
-  if (undefined == stations) {
-    return [];
-  }
-  return JSON.parse(stations);
-}
-function setFavourites(stations) {
-  localStorage["mystations"] = JSON.stringify(stations);
-}
-function clearFavourites() {
-  localStorage.removeItem("mystations");
-}
 function getStationUrl() {
   if (location.hostname === "localhost") {
     return "stations.json";
@@ -47,8 +34,55 @@ function getStationUrl() {
   return "https://mycitibike.nyc3.cdn.digitaloceanspaces.com/stations.json";
 }
 
+function getFavouriteState() {
+  f = localStorage.getItem("showFavourites");
+  if (undefined == stations) {
+    return false;
+  }
+  if (f == 1) {
+    return true;
+  }
+  return false;
+}
+function setShowFavourites() {
+  localStorage.setItem("showFavourites", 1);
+}
+function removeShowFavourites() {
+  localStorage.setItem("showFavourites", 0);
+}
+function getFavourites() {
+  stations = localStorage.getItem("mystations");
+  console.log(stations);
+  if (undefined == stations) {
+    return [];
+  }
+  return JSON.parse(stations);
+}
+function setFavourites(stations) {
+  localStorage.setItem("mystations", JSON.stringify(stations));
+}
+function clearFavourites() {
+  localStorage.removeItem("mystations");
+}
+function addFavourite(station) {
+  f = getFavourites();
+  if (f.includes(station)) {
+    return;
+  }
+  f.push(station);
+  setFavourites(f);
+}
+function removeFavourite(station) {
+  f = getFavourites();
+  if (f.includes(station)) {
+    x = f.filter(function (item) {
+      return item !== station;
+    });
+    setFavourites(x);
+  }
+}
 
-function sortByDist(rawData, position){
+function sortByDist(rawData, position) {
   lat = position.coords.latitude;
   lon = position.coords.longitude;
   for (var n = 0; n < rawData.length; n++) {
@@ -59,27 +93,122 @@ function sortByDist(rawData, position){
       lon
     ).toFixed(1);
   }
-  return sortByKey(rawData,"dist");
+  return sortByKey(rawData, "dist");
 }
 
-
-function renderTable(rawData){
+function renderTable1(rawData) {
   $("table").bootstrapTable({
     data: rawData,
+    onClickRow: function (row, $element) {},
   });
 }
 
-$(document).ready(function () {
-  mystations = getFavourites();
-  $.getJSON(getStationUrl(), function (rawData) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      rawData = sortByDist(rawData,position).slice(0,10);
-      if (mystations.length > 0) {
-        rawData = rawData.filter((item) =>
-          mystations.includes(item.station_id)
-        );
+function operateFormatter1(value, row, index) {
+  return [
+    '<a class="like" href="javascript:void(0)" title="Like">',
+    '<i class="fa fa-heart"></i>',
+    "</a>  ",
+    '<a class="remove" href="javascript:void(0)" title="Remove">',
+    '<i class="fa fa-trash"></i>',
+    "</a>",
+  ].join("");
+}
+
+function renderTable(rawData) {
+  $("table").bootstrapTable({
+    data: rawData,
+    onClickRow: function (i, j) {
+      var favouriteState = getFavouriteState();
+
+      if ("selected" === j.attr("class")) {
+        j.removeClass("selected");
+        if (getFavouriteState()) {
+          removeFavourite(i.i);
+        } else {
+          addFavourite(i.i);
+        }
+        var $table = $("#stationView");
+
+        if (favouriteState) {
+          $table.bootstrapTable("filterBy", {
+            i: getFavourites(),
+          });
+        } else {
+        }
+      } else {
+        j.addClass("selected");
+        if (getFavouriteState()) {
+          removeFavourite(i.i);
+        } else {
+          addFavourite(i.i);
+        }
+        var $table = $("#stationView");
+        if (favouriteState) {
+          $table.bootstrapTable("filterBy", {
+            i: getFavourites(),
+          });
+        } else {
+        }
       }
+    },
+  });
+  var $table = $("#stationView");
+  var favouriteState = getFavouriteState();
+
+  if (favouriteState) {
+    $table.bootstrapTable("filterBy", {
+      i: getFavourites(),
+    });
+  }
+}
+function getLocation(cb) {
+  if (location.hostname === "localhost") {
+    return cb({
+      coords: {
+        latitude: 1,
+        longditude: 1,
+      },
+    });
+  }
+  navigator.geolocation.getCurrentPosition(cb);
+}
+
+$(document).ready(function () {
+  var mystations = getFavourites();
+  var favouriteState = getFavouriteState();
+  var button = $("#toggleFavourites");
+
+  $.getJSON(getStationUrl(), function (rawData) {
+    getLocation(function (position) {
+      rawData = sortByDist(rawData, position).slice(0, 10);
       renderTable(rawData);
+      if (favouriteState) {
+        button.removeClass("btn-secondary");
+        button.addClass("btn-primary");
+      }
+    });
+  });
+
+  //https://examples.bootstrap-table.com/#methods/filter-by.html#view-source
+  var $table = $("#stationView");
+  var $button = $("#toggleFavourites");
+  $(function () {
+    $button.click(function () {
+      var checkbox = $(this);
+      console.log(checkbox);
+      if (checkbox.hasClass("btn-secondary")) {
+        checkbox.removeClass("btn-secondary");
+        checkbox.addClass("btn-primary");
+        setShowFavourites();
+        $table.bootstrapTable("filterBy", {
+          i: getFavourites(),
+        });
+      } else {
+        checkbox.removeClass("btn-primary");
+        checkbox.addClass("btn-secondary");
+        removeShowFavourites();
+        $table.bootstrapTable("filterBy", {});
+      }
     });
   });
 });
